388c5f3853cc995c6c9c1fc5c710692c54a12e2b
saitzaw <saitzaw>
2024-10-08 18:08:15 +0000

    feat: add scala and pyspark

--- SparkSQL/SQL Exercises 1.sql
+++ SparkSQL/SQL Exercises 1.sql
@@ -12,16 +12,16 @@ create schema if not exists db_demo;
 
 -- COMMAND ----------
 
-truncate table trees;
-
--- COMMAND ----------
-
 use schema db_demo;
 create table if not exists trees 
 (
   child integer
   , parent integer
   );
+
+-- COMMAND ----------
+
+truncate table trees;
 
 -- COMMAND ----------
 
@@ -91,6 +91,142 @@ from
   cte_root
 order by
   node_name;
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### PySpark
+-- MAGIC - create a dataframe for inner node 
+-- MAGIC - create a dataframe for leaf node 
+-- MAGIC - create a dataframe for root node 
+-- MAGIC - union all dataframe
+
+-- COMMAND ----------
+
+-- MAGIC %python 
+-- MAGIC from pyspark.sql import functions as F
+-- MAGIC treeDF = spark.sql("select * from trees")
+
+-- COMMAND ----------
+
+-- MAGIC %python 
+-- MAGIC treeDF.show()
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC #### Inner 
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC cloneTreeDF = treeDF.alias("cloneTree")
+-- MAGIC cloneTreeDF = cloneTreeDF.withColumnRenamed("child","c_child").withColumnRenamed("parent","c_parent")
+-- MAGIC # Perform the join using the aliased DataFrames
+-- MAGIC innerNodeDF = treeDF.join(
+-- MAGIC     cloneTreeDF, 
+-- MAGIC     treeDF["child"] == cloneTreeDF["c_parent"],
+-- MAGIC     'inner'
+-- MAGIC ).where(treeDF.parent.isNotNull()).select(
+-- MAGIC    cloneTreeDF["c_parent"]
+-- MAGIC ).distinct()
+-- MAGIC
+-- MAGIC # Display the result
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC #### leaf node
+
+-- COMMAND ----------
+
+-- MAGIC  %python
+-- MAGIC leafNodeDF =  treeDF.join(
+-- MAGIC     innerNodeDF, 
+-- MAGIC     treeDF["child"] == innerNodeDF["c_parent"],
+-- MAGIC     'leftanti'
+-- MAGIC ).where(treeDF.parent.isNotNull()).select(
+-- MAGIC    treeDF["child"]
+-- MAGIC ).distinct() 
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC leafNodeDF = leafNodeDF.withColumn("node_name", F.lit("leaf"))
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC #### root node
+
+-- COMMAND ----------
+
+-- MAGIC %python 
+-- MAGIC rootNodeDF = treeDF.where(treeDF.parent.isNull()).select("child").withColumn("node_name", F.lit("root"))
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC #### prepare for inner node 
+
+-- COMMAND ----------
+
+-- MAGIC %python 
+-- MAGIC innerNodeDFAlias = innerNodeDF.withColumnRenamed("c_parent", "child").withColumn("node_name", F.lit("inner"))
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC #### UNION
+
+-- COMMAND ----------
+
+-- MAGIC %python 
+-- MAGIC rootNodeDF.union(innerNodeDFAlias).union(leafNodeDF).show()
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Scala
+
+-- COMMAND ----------
+
+-- MAGIC %scala
+-- MAGIC import org.apache.spark.sql.functions.{lit, col}
+-- MAGIC val treeDFs = spark.sql("select * from dbx_sthz.db_demo.trees")
+
+-- COMMAND ----------
+
+-- MAGIC %scala 
+-- MAGIC treeDFs.show()
+
+-- COMMAND ----------
+
+-- MAGIC %scala 
+-- MAGIC val treeDFsAlias = treeDFs.withColumnRenamed("child", "c_child").withColumnRenamed("parent", "c_parent"); 
+-- MAGIC val  innerDFs = treeDFs.join(treeDFsAlias, treeDFs("child") === treeDFsAlias("c_parent"), "inner").where(treeDFs("parent").isNotNull).select(treeDFsAlias("c_parent")).distinct()
+
+-- COMMAND ----------
+
+-- MAGIC %scala 
+-- MAGIC val leafDFs = treeDFs.join(innerDFs, treeDFs("child") === innerDFs("c_parent"),"leftanti").where(treeDFs("parent").isNotNull).select(treeDFs("child"))
+
+-- COMMAND ----------
+
+-- MAGIC %scala 
+-- MAGIC val newInnerDFs = innerDFs.withColumnRenamed("c_parent", "child").withColumn("node_name", lit("inner"))
+-- MAGIC
+-- MAGIC
+
+-- COMMAND ----------
+
+-- MAGIC %scala
+-- MAGIC val rootDFs = treeDFs.where(treeDFs("parent").isNull).select(treeDFs("child")).withColumn("node_name", lit("root")) 
+
+-- COMMAND ----------
+
+-- MAGIC %scala 
+-- MAGIC rootDFs.union(newInnerDFs).union(leafDFs.withColumn("node_name", lit("leaf"))).show()
 
 -- COMMAND ----------
 
